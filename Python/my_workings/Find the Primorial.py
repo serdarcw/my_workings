af17312dfb1fe22c9098e0f92412606f27b398f2
serkangumus <serkangumus06@gmail.com>
2020-04-25 01:57:34 +0300

    Son olarak Matt ödevinde işlemin başa dönmesi eklendi

--- Python/my_workings/Find the Primorial.py
+++ Python/my_workings/Find the Primorial.py
@@ -1,9 +1,10 @@
-
-n = 50
-if n>48:
-    top =7000+(n%48)*600
-elif n>41:
-    top =2600+(n%32)*550
-elif n>32:
-    top =(n%32)*325
-print(top)
+  
+while True:
+    n = input("Please enter your number : ")
+    if not n.isdigit():
+        print("your entry is not valid entry, please use positive number")
+    else:
+        if sum([int(i)**len(n) for i in n]) == int(n):
+            print(f"{n} is an Armstrong number")
+        else:
+            print(f"{n} isn't an Armstrong number")
