b074688518117037319e79273f2a39304e44f214
serkangumus <serkangumus06@gmail.com>
2020-04-25 00:59:45 +0300

    armstrong number ödevi dahil birkaç python sorusu eklendi

--- Python/my_workings/Find the Primorial.py
+++ Python/my_workings/Find the Primorial.py
@@ -1,15 +1,9 @@
-#Find the Primorial
-#A Primorial is a product of the first n prime numbers (e.g. 2 x 3 x 5 = 30). 2, 3, 5, 7, 11, 13 are prime numbers. If n was 3, you'd multiply 2 x 3 x 5 = 30 or Primorial = 30.
-#
-#Create a function that returns the Primorial of a number.
-#
-#Examples
-#primorial(1) ➞ 2
-#
-#primorial(2) ➞ 6
-#
-#primorial(8) ➞ 9699690
 
-
-
-
+n = 50
+if n>48:
+    top =7000+(n%48)*600
+elif n>41:
+    top =2600+(n%32)*550
+elif n>32:
+    top =(n%32)*325
+print(top)
