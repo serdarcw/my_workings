bcbeea687223c5c623a9aaab412404a38e375741
serkangumus <serkangumus06@gmail.com>
2020-05-04 16:55:20 +0300

    prime num sorusu_son

--- Python/Assignments/prime_num.py
+++ Python/Assignments/prime_num.py
@@ -1,12 +1,14 @@
-#while True:
-
 num=int(input("please enter your number : "))
 count=0
-for i in range(2,round((num)**(0.5))):
-    if num%i==0:
-        count+=1
-if count>1:
+if num==2:
+    print(f"{num} is a prime number")
+elif num==1:
     print(f"{num} isn't a prime number")
 else:
-    print(f"{num} is a prime number")
-
+    for i in range(2,num):
+        if num%i==0:
+            count+=1
+    if count>1:
+        print(f"{num} isn't a prime number")
+    else:
+        print(f"{num} is a prime number")
